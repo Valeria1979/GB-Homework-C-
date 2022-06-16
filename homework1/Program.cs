6ffeee40af4de05f910e26601e0d21cdb4d63d02
Valeria Vodolazova <vvv91@internet.ru>
2022-06-16 17:32:19 +0300

    solved and commited problem4

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -54,3 +54,18 @@ else
 
 }
 */
+
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+/*
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int namber = 1;
+
+while(namber <= N)
+
+{
+    Console.WriteLine($"{namber = namber+1}");
+    namber++;
+}
+
+*/
