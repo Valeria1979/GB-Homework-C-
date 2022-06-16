6b19b1288b2dd1bb495d37ba77091ed499c014ec
Valeria Vodolazova <vvv91@internet.ru>
2022-06-16 15:24:16 +0300

    solved problem2

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -19,3 +19,19 @@ else
 
 */
 
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите три числа: ");
+int namberA = int.Parse(Console.ReadLine());
+int namberB = int.Parse(Console.ReadLine());
+int namberC = int.Parse(Console.ReadLine());
+int max = namberA;
+
+if(namberB > max) max = namberB;
+
+if(namberC > max) max = namberC;
+
+{
+    Console.WriteLine($"число {max} - максимальное число");
+}
+
