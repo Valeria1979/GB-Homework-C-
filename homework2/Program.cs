cc19529c876922283d386c610f62e4bff2be40c8
Valeria Vodolazova <vvv91@internet.ru>
2022-06-21 09:53:18 +0300

    added problem solving

---
+++ homework2/Program.cs
@@ -0,0 +1,77 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное 
+//число и на выходе показывает вторую цифру этого числа.
+
+/*Console.WriteLine("Введите трехзначное число: ");
+int namber = int.Parse(Console.ReadLine());
+Console.WriteLine(namber / 10 % 10);
+*/
+
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+/*void Thirddigit (int num){
+    if (num > 99){
+        int current = num;
+        while (current > 999){
+            current = current / 10;
+        }
+        int thirdnum = current % 10;
+        Console.WriteLine(num + "->" + thirdnum);
+        }
+    else {
+            Console.WriteLine(num + " -> третьей цифры нет");
+        }
+    }
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+Thirddigit (number);
+*/
+
+//Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+/*Console.WriteLine("Введите число дня недели: ");
+int namber = int.Parse(Console.ReadLine());
+
+if(namber == 1)
+
+{
+    Console.WriteLine($"нет");
+}
+
+    if(namber == 2)
+    
+    {
+        Console.WriteLine($"нет");
+    }  
+
+        if(namber == 3)
+
+        {
+            Console.WriteLine($"нет");   
+        }
+
+            if(namber == 4)
+
+            {
+                Console.WriteLine($"нет");
+            }
+
+                if(namber == 5)
+
+                {
+                    Console.WriteLine($"нет");
+                }
+
+                    if(namber == 6)
+
+                    {
+                        Console.WriteLine($"да");
+                    }
+
+                        if(namber == 7)
+
+                        {
+                            Console.WriteLine($"да");
+                        }
+*/
+
