ef9450c27a95c504dff55e07a23389bdc8805867
Valeria Vodolazova <vvv91@internet.ru>
2022-06-27 16:40:13 +0300

    added a solution to problem 21

--- homework3/Program.cs
+++ homework3/Program.cs
@@ -32,5 +32,26 @@ else
 }
 */
 
+// 23  Напишите программу, которая принимает на вход координаты двух 
+// точек и находит расстояние между ними в 3D пространстве.
+
+/*Console.WriteLine("Imput the coordinates of the two points: ");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int z1 = int.Parse(Console.ReadLine());
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
+
+int length = (int) Math.Pow(x2-x1,2) + (int) Math.Pow(y2-y1,2) + (int) Math.Pow(z2-z1,2);
+double resalt = Math.Sqrt(length);
+Console.WriteLine($"The distance between two points is equal to {resalt}");
+*/
+
+
+
+
+
+
 
 
