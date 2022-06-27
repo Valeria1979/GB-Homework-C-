c46d3de23dfa08e8012f2bd016fa4a7237bda240
Valeria Vodolazova <vvv91@internet.ru>
2022-06-27 11:57:43 +0300

    added task 19

--- homework3/Program.cs
+++ homework3/Program.cs
@@ -1,4 +1,4 @@
-﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+﻿// 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
 
 /*Console.Write("Imput namber N: ");
@@ -12,4 +12,25 @@ while(resalt <= N)
 }
 */
 
-//
+// 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+/*Console.Write("Imput 5-digit number: ");
+int number = int.Parse(Console.ReadLine());
+int A = number/10/10/10/10;
+int B = number/10 / 10 / 10 % 10;
+int C = number/10 % 10;
+int D = number % 10;
+
+if(A == D && B == C)
+{
+    Console.WriteLine($"The number of polindroms entered");
+}
+else
+{
+    Console.WriteLine($"The number entered is not a polindroms");
+
+}
+*/
+
+
+
