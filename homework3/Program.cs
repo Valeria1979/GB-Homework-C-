81896090b528de72b38c33932f2416ee2da63728
Valeria Vodolazova <vvv91@internet.ru>
2022-06-27 10:56:41 +0300

    added task 23

---
+++ homework3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+/*Console.Write("Imput namber N: ");
+int N = int.Parse(Console.ReadLine());
+int resalt = 1;
+
+while(resalt <= N)
+{
+    Console.WriteLine(Math.Pow(resalt, 3));
+    resalt++;
+}
+*/
+
+//
